45d9fba378d5513fba7e3db89059f3aea9bcca62
tykim5931 <tykim5931@gmail.com>
2022-07-14 15:23:53 +0900

    initial commit

---
+++ options/options.js
@@ -0,0 +1,8 @@
+console.log("Hello Wrold");
+const title = document.querySelector("#title");
+console.log(title);
+const link = document.querySelector("a");
+
+link.addEventListener("click",()=>{
+    console.log("링크를 클릭했습니다...")
+});
