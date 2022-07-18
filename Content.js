5b29ba4297b80867602f507b6049c90cd5d80cac
asjay18 <sejin1314@korea.ac.kr>
2022-07-18 16:28:32 +0900

    Change block screen

--- Content.js
+++ Content.js
@@ -3,256 +3,143 @@
 
 const generateSTYLES = () => {
   return `<style>@import url(https://fonts.googleapis.com/css?family=opensans:500);
-  body {
-    background: #33cc99;
-    color: #fff;
-    font-family: "Open Sans", sans-serif;
-    max-height: 700px;
-    overflow: hidden;
+    body {
+      background: #15095c;
+      color: #fff;
+      font-family: "Open Sans", sans-serif;
+      max-height: 700px;
+      overflow: hidden;
   }
-  .c {
-    text-align: center;
-    display: block;
-    position: relative;
-    width: 80%;
-    margin: 100px auto;
+
+  .top{
+      width: 100%;
+      height: 60vh;
+      display: flex;
+      align-items: center;
+      justify-content: center;
+      margin-top: 2vh;
   }
-  ._404 {
-    font-size: 220px;
-    position: relative;
-    display: inline-block;
-    z-index: 2;
-    height: 250px;
-    letter-spacing: 15px;
+  .center{
+      position: absolute;
+      width: 30vh;
+      height: 30vh;
+      animation: spin-right 20s linear infinite;
+  }
+  .edge{
+      width: 8vh;
+      height: 8vh;
+      margin-top: -2vh;
+      margin-left: -2vh;
+  }
+  @keyframes spin-right {
+      100%{
+          transform: rotate(360deg);
+      }
+  }
+
+
+  .bottom {
+      text-align: center;
+      display: block;
+      position: relative;
+      width: 80%;
+      margin-left: 10%;
+      margin-top: 2vh;
   }
   ._1 {
-    text-align: center;
-    display: block;
-    position: relative;
-    letter-spacing: 12px;
-    font-size: 4em;
-    line-height: 80%;
+      text-align: center;
+      display: block;
+      position: relative;
+      letter-spacing: 12px;
+      font-size: 4em;
+      line-height: 80%;
+      padding: 10px;
   }
   ._2 {
-    text-align: center;
-    display: block;
-    position: relative;
-    font-size: 20px;
+      text-align: center;
+      display: block;
+      position: relative;
+      font-size: 20px;
+      padding: 10px;
   }
-  .text {
-    font-size: 70px;
-    text-align: center;
-    position: relative;
-    display: inline-block;
-    margin: 19px 0px 0px 0px;
-    /* top: 256.301px; */
-    z-index: 3;
-    width: 100%;
-    line-height: 1.2em;
-    display: inline-block;
-  }
-  
- 
-  .right {
-    float: right;
-    width: 60%;
-  }
-  
   hr {
-    padding: 0;
-    border: none;
-    border-top: 5px solid #fff;
-    color: #fff;
-    text-align: center;
-    margin: 0px auto;
-    width: 420px;
-    height: 10px;
-    z-index: -10;
+      padding: 0;
+      border: none;
+      border-top: 5px solid #fff;
+      color: #fff;
+      text-align: center;
+      margin: 0px auto;
+      width: 420px;
+      height: 10px;
+      z-index: -10;
   }
-  
   hr:after {
-    display: inline-block;
-    position: relative;
-    top: -0.75em;
-    font-size: 2em;
-    padding: 0 0.2em;
-    background: #33cc99;
+      display: inline-block;
+      position: relative;
+      top: -0.75em;
+      font-size: 2em;
+      padding: 0 0.2em;
+      background: #33cc99;
   }
-  
-  .cloud {
-    width: 350px;
-    height: 120px;
-  
-    background: #fff;
-    background: linear-gradient(top, #fff 100%);
-    background: -webkit-linear-gradient(top, #fff 100%);
-    background: -moz-linear-gradient(top, #fff 100%);
-    background: -ms-linear-gradient(top, #fff 100%);
-    background: -o-linear-gradient(top, #fff 100%);
-  
-    border-radius: 100px;
-    -webkit-border-radius: 100px;
-    -moz-border-radius: 100px;
-  
-    position: absolute;
-    margin: 120px auto 20px;
-    z-index: -1;
-    transition: ease 1s;
-  }
-  
-  .cloud:after,
-  .cloud:before {
-    content: "";
-    position: absolute;
-    background: #fff;
-    z-index: -1;
-  }
-  
-  .cloud:after {
-    width: 100px;
-    height: 100px;
-    top: -50px;
-    left: 50px;
-  
-    border-radius: 100px;
-    -webkit-border-radius: 100px;
-    -moz-border-radius: 100px;
-  }
-  
-  .cloud:before {
-    width: 180px;
-    height: 180px;
-    top: -90px;
-    right: 50px;
-  
-    border-radius: 200px;
-    -webkit-border-radius: 200px;
-    -moz-border-radius: 200px;
-  }
-  
-  .x1 {
-    top: -50px;
-    left: 100px;
-    -webkit-transform: scale(0.3);
-    -moz-transform: scale(0.3);
-    transform: scale(0.3);
-    opacity: 0.9;
-    -webkit-animation: moveclouds 15s linear infinite;
-    -moz-animation: moveclouds 15s linear infinite;
-    -o-animation: moveclouds 15s linear infinite;
-  }
-  
-  .x1_5 {
-    top: -80px;
-    left: 250px;
-    -webkit-transform: scale(0.3);
-    -moz-transform: scale(0.3);
-    transform: scale(0.3);
-    -webkit-animation: moveclouds 17s linear infinite;
-    -moz-animation: moveclouds 17s linear infinite;
-    -o-animation: moveclouds 17s linear infinite;
-  }
-  
-  .x2 {
-    left: 250px;
-    top: 30px;
-    -webkit-transform: scale(0.6);
-    -moz-transform: scale(0.6);
-    transform: scale(0.6);
-    opacity: 0.6;
-    -webkit-animation: moveclouds 25s linear infinite;
-    -moz-animation: moveclouds 25s linear infinite;
-    -o-animation: moveclouds 25s linear infinite;
-  }
-  
-  .x3 {
-    left: 250px;
-    bottom: -70px;
-  
-    -webkit-transform: scale(0.6);
-    -moz-transform: scale(0.6);
-    transform: scale(0.6);
-    opacity: 0.8;
-  
-    -webkit-animation: moveclouds 25s linear infinite;
-    -moz-animation: moveclouds 25s linear infinite;
-    -o-animation: moveclouds 25s linear infinite;
-  }
-  
-  .x4 {
-    left: 470px;
-    botttom: 20px;
-  
-    -webkit-transform: scale(0.75);
-    -moz-transform: scale(0.75);
-    transform: scale(0.75);
-    opacity: 0.75;
-  
-    -webkit-animation: moveclouds 18s linear infinite;
-    -moz-animation: moveclouds 18s linear infinite;
-    -o-animation: moveclouds 18s linear infinite;
-  }
-  
-  .x5 {
-    left: 200px;
-    top: 300px;
-  
-    -webkit-transform: scale(0.5);
-    -moz-transform: scale(0.5);
-    transform: scale(0.5);
-    opacity: 0.8;
-  
-    -webkit-animation: moveclouds 20s linear infinite;
-    -moz-animation: moveclouds 20s linear infinite;
-    -o-animation: moveclouds 20s linear infinite;
-  }
-  
-  @-webkit-keyframes moveclouds {
-    0% {
-      margin-left: 1000px;
+
+  .btn{
+      color: #fff;
+      border: none;
+      margin: 20px;
+      padding: 20px 30px;
+      font-size: 18px;
+      font-weight: 600;
+      text-transform: uppercase;
+      letter-spacing: 4px;
+      cursor: pointer;
+      border-radius: 5px;
+      transition: 0.5s;
+      transition-property: box-shadow;
     }
-    100% {
-      margin-left: -1000px;
+    
+    .btn-primary{
+      background: #50A7FF;
+      box-shadow: 0 0 25px #50A7FF;
     }
-  }
-  @-moz-keyframes moveclouds {
-    0% {
-      margin-left: 1000px;
+    
+    .btn-primary:hover{
+      box-shadow: 0 0 5px #50A7FF,
+                  0 0 25px #50A7FF,
+                  0 0 50px #50A7FF,
+                  0 0 100px #50A7FF;
     }
-    100% {
-      margin-left: -1000px;
-    }
-  }
-  @-o-keyframes moveclouds {
-    0% {
-      margin-left: 1000px;
-    }
-    100% {
-      margin-left: -1000px;
-    }
-  }
    </style>`;
 };
 
 
 const generateHTML = (pageName) => {
-  return `
-   
-   <div id="clouds">
-      <div class="cloud x1"></div>
-      <div class="cloud x1_5"></div>
-      <div class="cloud x2"></div>
-      <div class="cloud x3"></div>
-      <div class="cloud x4"></div>
-      <div class="cloud x5"></div>
-  </div>
-  <div class='c'>
-      <div class='_404'>404</div>
-      <hr>
-      <div class='_1'>GET BACK TO WORK</div>
-      <div class='_2'>STUDYING > ${pageName}</div>
-  </div>
-  <button type="button" class="btn btn-primary active" id="gameBtn">test</button>
+  return `<!DOCTYPE html>
+  <html>
+      <head>
+          <meta charset="utf-8">
+          <meta http-equiv="X-UA-Compatible" content="IE=edge">
+          <title>ScreenSaver</title>
+          <meta name="description" content="">
+          <meta name="viewport" content="width=device-width, initial-scale=1">
+          <link rel="stylesheet" href="screen.css">
+      </head>
+      <body>
+          <div class="top">
+              <div class="center">
+                  <img src="49.png" class="center" alt="">
+                  <div class="edge">
+                      <img src="40.png" class="edge">
+                  </div>
+              </div>
+          </div>
+          <div class='bottom'>
+              <hr>
+              <div class='_1'>GET BACK TO WORK</div>
+              <div class='_2'>STUDYING > ${pageName}</div>
+              <button type="button" class="btn btn-primary active" id="gameBtn">게임해서 잠금해제</button>
+          </div>
+      </body>
+  </html>
    `;
 };
 
